88180000b6baacc7b24198b25d5d92b71f0da7f7
Pandora <pandora@qq.com>
2018-07-16 19:16:38 +0800

    chanage autoRun.sh file

--- autoRun.sh
+++ autoRun.sh
@@ -2,3 +2,32 @@
 
 cp ./.gitconfig ~
 cp ./.vimrc ~
+
+echo -e "1 -- git push"
+echo -e "2 -- git clone"
+
+read -p "Select you operation:" num
+
+url="https://github.com/pandora2018/c2018.git"
+
+if [ $num -eq 1 ]; then
+	read -p "input add file:" fileName
+	git add $fileName
+	
+	read -p "commit connect:" commit
+	git commit -m $commit
+	
+	git push $url 
+
+	if [ $? -eq 0 ]; then
+		echo " github is ok!"
+	fi
+
+elif [ $num -eq 2 ]; then
+	git clone $url 
+fi
+
+
+if [ $? -eq 0 ]; then
+	echo "shell script run successfully!"
+fi
