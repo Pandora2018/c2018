93946bc9af3d7c9c7b039f4852c869cad94a59d8
Pandora2018 <41287097+Pandora2018@users.noreply.github.com>
2018-07-17 10:23:10 +0800

    Update autoRun.sh

--- autoRun.sh
+++ autoRun.sh
@@ -1,33 +1,7 @@
 #!/bin/sh
 
+# install necessary software
+sudo apt install -y tmux > /dev/null
+
 cp ./.gitconfig ~
 cp ./.vimrc ~
-
-echo -e "1 -- git push"
-echo -e "2 -- git clone"
-
-read -p "Select you operation:" num
-
-url="https://github.com/pandora2018/c2018.git"
-
-if [ $num -eq 1 ]; then
-	read -p "input add file:" fileName
-	git add $fileName
-	
-	read -p "commit connect:" commit
-	git commit -m $commit
-	
-	git push $url 
-
-	if [ $? -eq 0 ]; then
-		echo " github is ok!"
-	fi
-
-elif [ $num -eq 2 ]; then
-	git clone $url 
-fi
-
-
-if [ $? -eq 0 ]; then
-	echo "shell script run successfully!"
-fi
