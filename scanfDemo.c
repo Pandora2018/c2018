f446b3e155c4679a3af3e41e3bbdbe98895327cc
Pandora <pandora@qq.com>
2018-07-21 14:21:05 +0800

    touch scanfDemo.c

--- scanfDemo.c
+++ scanfDemo.c
@@ -2,21 +2,40 @@
 
 int main(void)
 {
-	int count;
-	double assets;
-	char products[100];
-	unsigned char age;
-	size_t length;
+	int count = -1024;
+	double assets = -1024.76185;
+	char products[100] = "apple watch,iphone......";
 
-	printf("enter value:");
+	unsigned int field_width, point_precision, string_num;
+
+	// use * in the printf() function
+	printf("enter field width:");
+	scanf("%d", &field_width);
+	printf("[%*d]\n", field_width, count);
+
+	printf("enter field width and point precision:");
+	scanf("%d %d", &field_width, &point_precision);
+	printf("[%*.*f]\n", field_width, point_precision, assets);
+	
+	printf("enter field width and String number:");
+	scanf("%d %d", &field_width, &string_num);
+	printf("[%-*.*s]\n", field_width, string_num, products);
+
+	// use * in the scanf() function
+	char last_colors[10];
+	printf("enter three colors:");
+	scanf("%*d %*d %s", last_colors);
+	printf("Last Color is %s\n",last_colors);
+
+	/* printf("enter value:");
 	int total = scanf("%5lf %s", &assets, products);
 
 	printf("assets:%5.2f,products:%s\n",
 			assets, products);
-	printf("scanf() function return %d input date\n", total);
+	printf("scanf() function return %d input date\n", total); */
 
 	// print EOF value(EOF = end of file)
-	printf("EOF value:%d\n", EOF);
+	// printf("EOF value:%d\n", EOF);
 	
 	return 0;
 
