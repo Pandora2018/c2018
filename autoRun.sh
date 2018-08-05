1e2cb72abf7574277feda94596218f651651be97
Pandora <pandora@qq.com>
2018-08-05 10:40:33 +0800

    ..

--- autoRun.sh
+++ autoRun.sh
@@ -2,10 +2,14 @@
 
 # install necessary software
 sudo apt install -y tmux
+sudo apt install -y gdb
 
 cp -v ./.gitconfig ~
 cp -v ./.vimrc ~
 cp -v ./.tmux.config ~
 cp -v ./.gdbinit ~
+
+# alias
+alias la='la -A'
 
 echo "All operator running finish......"
