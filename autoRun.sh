626d5b2332f398c1607869f325b0e40c9ac8449a
Pandora <pandora@qq.com>
2018-08-16 18:55:32 +0800

    modify .vimrc

--- autoRun.sh
+++ autoRun.sh
@@ -11,5 +11,7 @@ cp -v ./.gdbinit ~
 
 # alias
 alias la='ls -lAh'
+alias gitpush='git push https://github.com/pandora2018/c2018.git'
+alias gitclone='git clone https://github.com/pandora2018/c2018.git'
 
 echo "All operator running finish......"
