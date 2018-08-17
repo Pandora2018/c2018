24e400f893c6f893388ff5b96f83c8827f0ad017
Pandora <pandora@qq.com>
2018-08-17 12:33:34 +0800

    ..

--- ch5_exercise.c
+++ ch5_exercise.c
@@ -6,17 +6,21 @@
 #define DAYS_PER_YEAR 360
 #define DAYS_PER_MONTH 30
 #define DAYS_PER_WEEK 7
+#define SEC_PER_HOUR 3600
+#define SEC_PER_MINUTE 60
 
 void add10();	// function prototype
 double real_num_cube(void);
 void data_convert(unsigned int days);
+void time_convert(unsigned int second);
 
 int main(void)
 {
 	// add10();
 	// printf("cube is %.3f\n", real_num_cube());
 	data_convert(120478);
-
+	time_convert(489756322);
+	
 	return 0;
 
 }
@@ -63,5 +67,17 @@ void data_convert(unsigned int days)
 
 	printf("%u days convert to %03u years %02u month %02u week %03u day\n",
 			days, year, month, weekday, day);
+
+}
+
+void time_convert(unsigned int second)
+{
+	unsigned int hour, minute, sec;
+
+	hour = second / SEC_PER_HOUR;
+	minute = second % SEC_PER_HOUR / SEC_PER_MINUTE;
+	sec = second % SEC_PER_HOUR % SEC_PER_MINUTE;
+
+	printf("%u second:%uh%um%u\n", second, hour, minute, sec);
 
 }
