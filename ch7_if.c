6fe1f13bccd644c5294a5239460fae075e50fbc7
Pandora <pandora@qq.com>
2018-09-17 19:14:10 +0800

    ..

--- ch7_if.c
+++ ch7_if.c
@@ -4,7 +4,25 @@ void readChar(void);
 
 int main(void)
 {
-	readChar();
+	// readChar();
+	char str[50] = "apple watch iphone ipad";
+
+	int index = 0;
+
+	// while (str[index] != '\0')
+	while (str[index])
+	{
+		if (str[index] != '\0')
+			printf("%c", str[index]);
+			// putchar(str[index]);
+		else
+		{
+			printf("index:%d\n", index);
+			break;
+		}
+
+		index++;
+	}
 
 	return 0;
 }
