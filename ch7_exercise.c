68c53b2cc7ecb4ce14264f2d37603e2d0a57840a
Pandora <pandora@qq.com>
2018-09-19 20:47:04 +0800

    ..

--- ch7_exercise.c
+++ ch7_exercise.c
@@ -2,11 +2,13 @@
 
 void guess(void);
 void char_to_ascii(void);
+void calc_odd_even(void);
 
 int main(void)
 {
 	/* guess(); */
-	char_to_ascii();
+	/* char_to_ascii(); */
+	calc_odd_even();
 	return 0;
 }
 
@@ -48,4 +50,44 @@ void char_to_ascii(void)
 		if (i % 8 == 0)
 			putchar('\n');
 	}
+}
+
+
+void calc_odd_even(void)
+{
+	unsigned long int num, sum_odd, sum_even;
+	int count_odd, count_even;
+
+	sum_odd = sum_even = 0;
+	count_odd = count_even =0;
+	
+	printf("Please input number from keyword to screen:\n");
+
+	while (scanf("%ld", &num) == 1)
+	{
+		/* if (0 == num)
+			break; */
+
+		if (num >= 1)
+		{
+			if (num % 2 == 0)
+			{
+				sum_even += num;
+				count_even++;
+			}else
+			{
+				sum_odd += num;
+				count_odd++;
+			}
+		}else
+		{
+			printf("num = 0\n");
+			break;
+		}
+
+		printf("next number:\n");
+	}			
+
+	printf("odd number :%d,sum_odd:%ld,average:%.4f\n", count_odd, sum_odd, 1.0 * sum_odd / count_odd);
+	printf("even number:%d,sum even:%ld,average:%.4f\n", count_even, sum_even, 1.0 * sum_even / count_even);
 }
