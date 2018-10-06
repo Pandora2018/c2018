01abfd4a4698efaa4c2801aa1e1c64435ea400d8
Pandora <pandora@qq.com>
2018-10-06 11:51:13 +0800

    ..

--- chapter8/eof.c
+++ chapter8/eof.c
@@ -11,14 +11,36 @@
 # ====================================================*/
 
 #include <stdio.h>
-
+#include <ctype.h>
 
 int main(void)
 {
 	int ch;
+	int cnt = 0;
 
 	while ((ch = getchar()) != EOF)
-		putchar(ch);
+	{
+		if (ch == '\n')
+			cnt++;
 
+		putchar(ch);
+	}
+
+	/* while (1)
+	{
+		ch = getchar();
+
+		if (isalpha(ch))
+			putchar(ch);
+		else if (isdigit(ch))
+			printf("%d", ch);
+		else if (ch == EOF)
+			break;
+		else
+			printf("\n");
+	} */
+
+	printf("------\n");
+	printf("\\n number:%d\n", cnt);
 	return 0;
 }
