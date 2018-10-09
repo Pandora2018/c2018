0516eb6c647743ec12b22af61cdc52c23c26cefe
Pandora <pandora@qq.com>
2018-10-09 13:32:11 +0800

    ..

--- chapter8/exerise.c
+++ chapter8/exerise.c
@@ -5,19 +5,22 @@
 #   Author        : Pandora
 #   Email         : pandora@github.com
 #   File Name     : exerise.c
-#   Last Modified : 2018-10-08 19:21
+#   Last Modified : 2018-10-09 12:59
 #   Describe      : this is chapter8 exerise
 #
 # ====================================================*/
 
 #include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
 
 void charcnt(void);
-void pri_ascii(void);
+void avg_char(void);
 
 int main(void)
 {
-	charcnt();
+	// charcnt();
+	avg_char();
 
 	return 0;
 }
@@ -37,7 +40,32 @@ void charcnt(void)
 }
 
 
-void pri_ascii(void)
+void avg_char(void)
 {
+	int ch;
+	int char_cnt, word_cnt;
+	bool isword = true;
 
+	char_cnt = word_cnt = 0;
+	
+	printf("enter string:\n");
+
+	while ((ch = getchar()) != EOF)
+	{
+		if (isalpha(ch))
+			char_cnt++;
+
+		if (! isspace(ch) && isword)
+		{
+			word_cnt++;
+			isword = false;
+		}else 
+
+		if (isspace(ch) && !isword)
+			isword = true;
+	}
+
+	printf("word count : %d\n", word_cnt);
+	printf("char count : %d\n", char_cnt);
+	printf("each word average length: %.2f\n", 1.0 * char_cnt / word_cnt);
 }
