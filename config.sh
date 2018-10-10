cb27e33c1420a1f40176e46a57405aac7d8f062c
Pandora <pandora@qq.com>
2018-10-10 16:34:38 +0800

    ..

--- config.sh
+++ config.sh
@@ -14,6 +14,5 @@ cp -v ./.gdbinit ~
 alias la='ls -lAh'
 alias gitpush='git push https://github.com/pandora2018/c2018.git'
 alias gitclone='git clone https://github.com/pandora2018/c2018.git'
-alias gitlog='git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative'
 
 echo "All operator running finish......"
