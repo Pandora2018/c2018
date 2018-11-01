b96c02aa0e9b41ebf4e7774f26f6d6c44b9ed905
Pandora <pandora@qq.com>
2018-11-01 18:52:09 +0800

    ..

--- chapter9/locaddress.c
+++ chapter9/locaddress.c
@@ -12,20 +12,10 @@
 
 #include <stdio.h>
 
-void locadd(double);
-void interchange(int, int);
+void interchange(int *, int *);
 
 int main(void)
 {
-	// double d1, d2;
-	// d1 = 1.00;
-	// d2 = 2.00;
-
-	// printf("in main() d1 = %f, d1 address :%p\n", d1, &d1);
-	// printf("in main() d2 = %f, d2 address :%p\n", d2, &d2);
-
-	// locadd(d1);
-
 	/*
 	 * sometime function can change other function varibale value,example called function must change calling function varibale value
 	 */
@@ -33,38 +23,28 @@ int main(void)
 
 	printf("originally a = %d and b = %d\n", a, b);
 
-	interchange(a, b);
+	interchange(&a, &b);
 
 	printf("Now a = %d and b = %d\n" ,a, b);
 
-	printf("-----\n");
-	printf("main() address %p, locadd() addredd %p,interchange() address %p\n", &main, &locadd, &interchange);
+	/* double price = 3.00; */
+	// double * ptr = &price;
+	
+	// printf("double * point use %zdByte\n", sizeof(ptr));
+	
+	/* printf("price = %.2f", *ptr) */;
 
 	return 0;
 }
 
 
-void locadd(double d1)
-{
-	double d2 = 2.00;
-
-	printf("in locadd() d1 = %f, d1 address :%p\n", d1, &d1);
-	printf("in locadd() d2 = %f, d2 address :%p\n", d2, &d2);
-
-	return;
-}
-
-
-void interchange(int x, int y)
+void interchange(int * x, int * y)
 {
 	int tmp;
-	printf("Originally x = %d and y = %d\n", x, y);
 
-	tmp = x;
-	x = y;
-	y = tmp;
-
-	printf("Now x = %d and y = %d\n", x, y);
+	tmp = *x;
+	*x = *y;
+	*y = tmp;
 
 	return;
 }
