e69439cd123949cab714e8b0ea43c6c1806688f6
Pandora <pandora@qq.com>
2018-12-17 14:15:53 +0800

    update

--- chapter11/is_within.c
+++ chapter11/is_within.c
@@ -20,12 +20,16 @@ int main(void)
 	puts("Please input string:");
 
 	while (s_gets(test, 20)) {
+		/* the newline is blankline,then skip */
+		if (*test == '\0') break;
+
 		puts("Please input check character:");
 		scanf("%c", &ch);
 		res = is_within(test, ch);
 		
 		printf("\'%c\' %s\n", ch, res ? "in the string" : "not in the string");
 
+		/* skip to string end */
 		while (getchar() != '\n')
 			continue;
 
