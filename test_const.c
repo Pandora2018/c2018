88334ec931eea27f0e387d64974e259833a40d46
Pandora <pandora@qq.com>
2020-02-22 10:38:15 +0800

    modify test_const.c

--- test_const.c
+++ test_const.c
@@ -12,24 +12,11 @@
 
 #include <stdio.h>
 
-// typedef const const const const short int a;
+extern const short int b = 12;
 
 int main(void)
 {
-	// const const const const short int a = 10;
-	// const a b = 100;
-	const short int b = 100;
-
-	// printf("a = %hd\n", a);
 	printf("b = %hd\n", b);
-
-
-	/*
-	 * float price = 2.12, num = 4.00;
-	 * float * const pf = &price;
-	 */
-	// pf = &num;	
-	// printf("price = %.2f\n", *pf);
 
 	return 0;
 }
