05038429d189cff1e35a42b4813350d4db758d96
Pandora <pandora@qq.com>
2020-02-28 16:39:06 +0800

    test fgets function

---
+++ chapter13/test_fgets.c
@@ -0,0 +1,27 @@
+/* ====================================================
+#   Copyright (C)2020 All rights reserved.
+#
+#   Author        : Pandora
+#   Email         : pandora@github.com
+#   File Name     : test_fgets.c
+#   Last Modified : 2020-02-28 16:34
+#   Describe      :
+#
+# ====================================================*/
+
+
+#include <stdio.h>
+#include <string.h>
+
+
+int main(void)
+{
+	char str[100];
+
+	printf("inputs string : ");
+	fgets(str, 80, stdin);
+
+	printf("%s", str);
+
+	return 0;
+}
