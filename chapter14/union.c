c6890759329491bd69a7594a7e3e0dafc7b3ddc8
Pandora <pandora@qq.com>
2020-07-10 20:12:49 +0800

    Test union data type

--- chapter14/union.c
+++ chapter14/union.c
@@ -12,8 +12,18 @@
 #include <stdio.h>
 #include "union.h"
 
+union woman
+{
+	unsigned char age;
+	char sex[1];
+	double height;
+};
+
+typedef union woman woman;
+
 int main(void)
 {
+#if 0
 	union tree t = {0x12345678};
 
 	/*
@@ -26,6 +36,17 @@ int main(void)
 	/* printf("t.flag = %d\n", t.flag); */
 	/* printf("t.height = %.2f\n", t.height); */
 	/* printf("p->height : %.2f\n", p->height); */
+#endif
+	
+	woman Lily;
+	Lily.age = 112;
+	Lily.height = 2.12;
 
+	printf("Lily's age:%hhu\n", Lily.age);
+	printf("Lily's sex:%s\n", Lily.sex);
+	printf("Lily's height:%.2f\n", Lily.height);
+
+	printf("union woman use memory %zd byte\n", sizeof(woman));
+	
 	return 0;
 }
